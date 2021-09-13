17edb24bf60a0f03747145ab24db57a8f0cabd14
UnicodeRogue <49918839+UnicodeRogue@users.noreply.github.com>
2021-09-13 07:52:33 -0400

    add calc.s to display from seconds to minutes

--- timer.js
+++ timer.js
@@ -17,3 +17,51 @@ pauseButton.addEventListener('click', () => {
 stopButton.addEventListener('click', () => {
   toggleClock(true)
 })
+
+let isClockRunning = false
+
+// in seconds = 25 mins
+let workSessionDuration = 1500
+let currentTimeLeftInSession = 1500
+
+// in seconds = 5 mins;
+let breakSessionDuration = 300
+
+const toggleClock = (reset) => {
+  if (reset) {
+    // STOP THE TIMER
+  } else {
+    if (isClockRunning === true) {
+      clockTimer = setInterval(() => {
+       // decrease time left / increase time spent
+        currentTimeLeftInSession--
+        displayCurrentTimeLeftInSession()
+        }, 1000)
+      
+      // PAUSE THE TIMER
+      clearInterval(clockTimer)
+      isClockRunning = false
+    } else {
+      // START THE TIMER
+      isClockRunning = true
+    }
+  }
+}
+
+const displayCurrentTimeLeftInSession = () => {
+  const secondsLeft = currentTimeLeftInSession
+  let result = ''
+  const seconds = secondsLeft % 60
+  const minutes = parseInt(secondsLeft / 60) % 60
+  let hours = parseInt(secondsLeft / 3600)
+  // add leading zeroes if it's less than 10
+  function addLeadingZeroes(time) {
+    return time < 10 ? `0${time}` : time
+  }
+  if (hours > 0) result += `${hours}:`
+  result += `${addLeadingZeroes(minutes)}:${addLeadingZeroes(seconds)}`
+  pomodoroTimer.innerText = result.toString()
+}
+
+pomodoroTimer.innerText = result
+
