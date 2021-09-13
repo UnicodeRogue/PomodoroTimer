81b9a5df7ad3089c8b3cd6e451f9fcd2c10f9eb7
UnicodeRogue <49918839+UnicodeRogue@users.noreply.github.com>
2021-09-13 08:01:44 -0400

    adding more to stop and start

--- timer.js
+++ timer.js
@@ -24,6 +24,7 @@ let type = 'Work'
 // in seconds = 25 mins
 let workSessionDuration = 1500
 let currentTimeLeftInSession = 1500
+let timeSpentInCurrentSession = 0
 
 // in seconds = 5 mins;
 let breakSessionDuration = 300
@@ -70,20 +71,26 @@ pomodoroTimer.innerText = result
 
 const stopClock = () => {
   // 1) reset the timer we set
+  displaySessionLog(type)
   clearInterval(clockTimer)
   // 2) update our variable to know that the timer is stopped
   isClockRunning = false
   // reset the time left in the session to its original state
   currentTimeLeftInSession = workSessionDuration
+  timeSpentInCurrentSession = 0
   // update the timer displayed
   displayCurrentTimeLeftInSession()
+  type = 'Work'
+  // could do it this way as a toggle: type = type === 'Work' ? 'Break' : 'Work'
 }
 
 const stepDown = () => {
   if (currentTimeLeftInSession > 0) {
     // decrease time left / increase time spent
     currentTimeLeftInSession--
+    timeSpentInCurrentSession++
   } else if (currentTimeLeftInSession === 0) {
+    timeSpentInCurrentSession = 0
     // Timer is over -> if work switch to break, viceversa
     if (type === 'Work') {
       currentTimeLeftInSession = breakSessionDuration
@@ -96,4 +103,16 @@ const stepDown = () => {
     }
   }
   displayCurrentTimeLeftInSession()
+}
+
+const displaySessionLog = (type) => {
+  const sessionsList = document.querySelector('#pomodoro-sessions')
+  // append li to it
+  const li = document.createElement('li')
+  let sessionLabel = type
+  let elapsedTime = parseInt(timeSpentInCurrentSession / 60)
+  elapsedTime = elapsedTime > 0 ? elapsedTime : '< 1'
+  const text = document.createTextNode(`${sessionLabel} : ${elapsedTime} min`)
+  li.appendChild(text)
+  sessionsList.appendChild(li)
 }
