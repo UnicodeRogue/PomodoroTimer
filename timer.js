0dca8932b36cffa44a8aab0cba79187234cc1cd7
UnicodeRogue <49918839+UnicodeRogue@users.noreply.github.com>
2021-09-13 07:56:10 -0400

    adding more complexity

--- timer.js
+++ timer.js
@@ -19,6 +19,7 @@ stopButton.addEventListener('click', () => {
 })
 
 let isClockRunning = false
+let type = 'Work'
 
 // in seconds = 25 mins
 let workSessionDuration = 1500
@@ -29,14 +30,16 @@ let breakSessionDuration = 300
 
 const toggleClock = (reset) => {
   if (reset) {
+    stopClock()
     // STOP THE TIMER
+    
   } else {
     if (isClockRunning === true) {
-      clockTimer = setInterval(() => {
-       // decrease time left / increase time spent
-        currentTimeLeftInSession--
-        displayCurrentTimeLeftInSession()
-        }, 1000)
+     clockTimer = setInterval(() => {
+      // decrease time left / increase time spent
+      stepDown()
+      displayCurrentTimeLeftInSession()
+      }, 1000)
       
       // PAUSE THE TIMER
       clearInterval(clockTimer)
@@ -65,3 +68,32 @@ const displayCurrentTimeLeftInSession = () => {
 
 pomodoroTimer.innerText = result
 
+const stopClock = () => {
+  // 1) reset the timer we set
+  clearInterval(clockTimer)
+  // 2) update our variable to know that the timer is stopped
+  isClockRunning = false
+  // reset the time left in the session to its original state
+  currentTimeLeftInSession = workSessionDuration
+  // update the timer displayed
+  displayCurrentTimeLeftInSession()
+}
+
+const stepDown = () => {
+  if (currentTimeLeftInSession > 0) {
+    // decrease time left / increase time spent
+    currentTimeLeftInSession--
+  } else if (currentTimeLeftInSession === 0) {
+    // Timer is over -> if work switch to break, viceversa
+    if (type === 'Work') {
+      currentTimeLeftInSession = breakSessionDuration
+      displaySessionLog('Work')
+      type = 'Break'
+    } else {
+      currentTimeLeftInSession = workSessionDuration
+      type = 'Work'
+      displaySessionLog('Break')
+    }
+  }
+  displayCurrentTimeLeftInSession()
+}
